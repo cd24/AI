c2695953e7a9553893432daf6e40ebfc70de9f08
John McAvey <john@mcavey.net>
2015-09-01 18:02:09 -0500

    Fixed problem of seperate maze tests
    All heuristics run on the same maze for each round of testing.

--- src/maze/core/MazeTest.java
+++ src/maze/core/MazeTest.java
@@ -2,6 +2,7 @@ package maze.core;
 
 import static org.junit.Assert.*;
 
+import junit.framework.TestResult;
 import maze.gui.AIReflector;
 import org.junit.Test;
 
@@ -10,12 +11,17 @@ import search.core.BestFirstSearcher;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class MazeTest {
 	final static int NUM_TESTS = 100;
-	final static int WIDTH = 10, HEIGHT = 15;
+	static int WIDTH = 10, HEIGHT = 15, NUM_TREASURES = 0;
+	static double PERFECTION = 50;
+	static String NAME = "No Treasure Tests";
 	final static String homeDir = System.getProperty("user.home") + File.separator + "Desktop",
 			customFunctionPerformanceFile = "UserPerformanceResults.csv" ;
+
+	static HashMap<String, ArrayList<TestResult>> results = new HashMap<>();
 
 	@Test
 	public void testNoTreasure() {
@@ -69,33 +75,129 @@ public class MazeTest {
 	@Test
 	public void testUserDefined() throws ClassNotFoundException, IllegalAccessException, InstantiationException, IOException {
 		PrintWriter writer = new PrintWriter(homeDir + File.separator + customFunctionPerformanceFile, "UTF-8");
-		String prefix = "maze.heuristics";
-		AIReflector reflector = new AIReflector(BestFirstHeuristic.class, prefix);
-		ArrayList<String> names = reflector.getTypeNames();
+		testWithParameters(writer);
 
-		for (String name : names){
-			writeToFile("nodes, depth, b*, solution length, " + name, writer);
-			String qualifiedName = prefix + "." + name;
-			BestFirstHeuristic<MazeExplorer> instance = (BestFirstHeuristic<MazeExplorer>) Class.forName(qualifiedName).newInstance();
-			testUserHeuristic(instance, writer);
+		for (double i = 0.25; i <= 1; i += 0.25){
+			configurePerfections(i);
+			testWithParameters(writer);
+		}
+
+		for (int i = 0; i < 20; i += 10){
+			configureTreasure(i, true);
+			testWithParameters(writer);
 		}
 		writer.close();
 	}
 
-	public void testUserHeuristic(BestFirstHeuristic hr, PrintWriter writer) throws IOException {
+	public void configureTreasure(int num_treasures, boolean perfect){
+		NAME = "Treasure Tests";
+		NUM_TREASURES = num_treasures;
+		PERFECTION = perfect ? 100 : 50;
+	}
+
+	public void configurePerfections(double perfection){
+		NAME = "Perfection Test";
+		NUM_TREASURES = 5;
+		PERFECTION = perfection;
+	}
+
+	public void setMazeDimensions(int width, int height){
+		WIDTH = width;
+		HEIGHT = height;
+	}
+
+	public void testWithParameters(PrintWriter writer) throws IOException, ClassNotFoundException, IllegalAccessException, InstantiationException {
+
+		String prefix = "maze.heuristics";
+		AIReflector reflector = new AIReflector(BestFirstHeuristic.class, prefix);
+		ArrayList<String> names = reflector.getTypeNames();
+
 		for (int i = 0; i < NUM_TESTS; ++i) {
 			Maze m = new Maze(WIDTH, HEIGHT);
-			m.makeMaze(new MazeCell(0, 0), new MazeCell(WIDTH - 1, HEIGHT - 1), 0, 1);
-			BestFirstSearcher<MazeExplorer> bestFirst = new BestFirstSearcher<>(hr);
-			MazeExplorer startNode = new MazeExplorer(m, m.getStart());
-			MazeExplorer endNode = new MazeExplorer(m, m.getEnd());
-			bestFirst.solve(startNode, endNode);
-			assertTrue(bestFirst.success());
-			writeToFile(bestFirst.getNumNodes() + ", " + bestFirst.getMaxDepth() + ", " + bestFirst.getBranchingFactor(Double.MAX_VALUE) + ", " + bestFirst.numSteps(), writer);
+			m.makeMaze(new MazeCell(m.getXMax(), m.getYMin()),
+					new MazeCell(m.getXMin(), m.getYMax()),
+					NUM_TREASURES,
+					PERFECTION);
+			for (String name : names) {
+				if (!results.containsKey(name)){
+					results.put(name, new ArrayList<>());
+				}
+				String qualifiedName = prefix + "." + name;
+				BestFirstHeuristic<MazeExplorer> instance = (BestFirstHeuristic<MazeExplorer>) Class.forName(qualifiedName).newInstance();
+				testUserHeuristic(instance, results.get(name), m);
+			}
 		}
+		writeResultsToFile(writer);
+		reset();
+	}
+
+	public void testUserHeuristic(BestFirstHeuristic hr, ArrayList<TestResult> res, Maze m) throws IOException {
+		m.makeMaze(new MazeCell(0, 0), new MazeCell(WIDTH - 1, HEIGHT - 1), 0, 1);
+		BestFirstSearcher<MazeExplorer> bestFirst = new BestFirstSearcher<>(hr);
+		MazeExplorer startNode = new MazeExplorer(m, m.getStart());
+		MazeExplorer endNode = new MazeExplorer(m, m.getEnd());
+		bestFirst.solve(startNode, endNode);
+
+		if(bestFirst.success()){
+			double b = bestFirst.getBranchingFactor(0.01);
+			b = (double)((int)(b * 100)) / 100;
+			TestResult result = new TestResult(bestFirst.getNumNodes(), bestFirst.getMaxDepth(), b, bestFirst.numSteps());
+			res.add(result);
+		}
+		else{
+			res.add(new TestResult(false));
+		}
+	}
+
+	public void writeResultsToFile(PrintWriter writer){
+		writer.println(NAME + ", "  + "Maze (W " + WIDTH + "|H " + HEIGHT + "), Number Treasures: " + NUM_TREASURES + ", Maze Perfection: " + PERFECTION);
+		writer.println(",nodes, depth, b*, solution length (Unreliable -- Averaging problem), times failed, number of trials");
+
+		for (String heuristic : results.keySet()){
+			double num_nodes = 0, max_depth = 0, branching_factor = 0, num_steps = 0;
+			int times_failed = 0, num_trials = results.get(heuristic).size();
+			for (TestResult result: results.get(heuristic)){
+				if (result.succeeded){
+					num_nodes += result.num_nodes;
+					max_depth += result.max_depth;
+					branching_factor += result.branching_factor;
+					num_steps += result.num_steps;
+				}
+				else{
+					times_failed += 1;
+					break;
+				}
+			}
+			num_nodes = num_nodes/num_trials;
+			max_depth = max_depth/num_trials;
+			branching_factor = branching_factor/num_trials;
+			num_steps = num_steps/num_trials;
+
+			writer.println(heuristic + "," + num_nodes + ", " + max_depth + ", " + branching_factor + ", " + num_steps + ", " + times_failed + ", " + num_trials);
+		}
+	}
+
+	public void reset(){
+		results = new HashMap<>();
 	}
 
 	public void writeToFile(String line, PrintWriter fos) throws IOException {
 		fos.println(line);
+	}
+
+	public static class TestResult {
+		public double num_nodes, max_depth, branching_factor, num_steps, completeness = 100;
+		public boolean succeeded = true;
+		public int treasure_count = 0;
+		String type = "No Treasure";
+		public TestResult(double nodes, double depth, double branching_factor, double num_steps){
+			this.num_nodes = nodes;
+			this.max_depth = depth;
+			this.branching_factor = branching_factor;
+			this.num_steps = num_steps;
+		}
+		public TestResult(boolean succeeded){
+			this.succeeded = succeeded;
+		}
 	}
 }
